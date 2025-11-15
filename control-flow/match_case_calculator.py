28d7117ae69e7e68ad0cf6f1f95516a69a8d3310
David Olarinde <olarindedavid469@gmail.com>
2025-11-16 08:22:54 +1200

    Fixed checker issues

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,7 +3,7 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 #Ask the user to enter an operator
-operation = input("Enter an operator (+, -, *, /): ")
+operation = input("Choose the operator (+, -, *, /):")
 
 #Use match case to perform the operation
 match operation:
@@ -23,4 +23,4 @@ match operation:
             result = num1 / num2
             print(f"The result is {result}")
     case _:
-        print("Invalid operator. Please enter one of +, -, *, /.")
+        print("Invalid operator.")
